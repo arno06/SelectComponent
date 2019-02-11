a9cd0dc9c8aa5020f161a96ce7f8509b2d01de00
Arnaud NICOLAS <arnaud.nicolas@vidal.fr>
2019-02-11 16:12:51 +0100

    Gestion de l'ordre

--- src/SelectComponent.js
+++ src/SelectComponent.js
@@ -1,7 +1,8 @@
 
-function SelectComponent(pElement, pOptions){
+function SelectComponent(pElement){
     this.element = pElement;
     this.element.setAttribute("multiple", "multiple");
+    this.element.style.display = "none";
     this.parent = this.element.parentNode;
     this.options = this.element.querySelectorAll("option");
     var ct = 1;
@@ -21,17 +22,20 @@ Class.define(SelectComponent, [EventDispatcher], {
         var ref = this;
         var availableOptions = "<div class='scomponent-options available'>" + (this.unselectedOptions.map(function(pOption){
                 return "<div class='option' data-value='"+pOption.getAttribute("value")+"' data-id='"+pOption.getAttribute("id")+"'>"+pOption.innerHTML+"</div>";
-            })).join("") + '</div>';
+            })).join("") + '<div></div></div>';
         var selectedOptions = "<div class='scomponent-options selected'>" + (this.selectedOptions.map(function(pOption){
                 return "<div class='option' data-value='"+pOption.getAttribute("value")+"' data-id='"+pOption.getAttribute("id")+"'>"+pOption.innerHTML+"</div>";
-            })).join("") + '</div>';
+            })).join("") + '<div></div></div>';
         this.parent.innerHTML += "<div class='scomponent-container'>" +
             availableOptions +
             "<div class='scomponent-actions'><button class='select'>&gt;</button><button class='unselect'>&lt;</button></div>" +
             selectedOptions +
+            "<div class='scomponent-actions'><button class='up'>&and;</button><button class='down'>&or;</button></div>" +
             "</div>";
         this.parent.querySelector("button.select").addEventListener('click', this.select.bind(this));
         this.parent.querySelector("button.unselect").addEventListener('click', this.unselect.bind(this));
+        this.parent.querySelector("button.up").addEventListener('click', this.up.bind(this));
+        this.parent.querySelector("button.down").addEventListener('click', this.down.bind(this));
         this.parent.querySelectorAll(".scomponent-options>div.option").forEach(function(pOption){
             pOption.addEventListener('click', ref.toggleOptionSelectionHandler.bind(ref), false);
         });
@@ -41,7 +45,7 @@ Class.define(SelectComponent, [EventDispatcher], {
         var ref = this;
         this.parent.querySelectorAll(".scomponent-options."+pFrom+" .option.selected").forEach(function(pItem){
             pItem.parentNode.removeChild(pItem);
-            ref.parent.querySelector(".scomponent-options."+pTo).appendChild(pItem);
+            ref.parent.querySelector(".scomponent-options."+pTo).insertBefore(pItem, ref.parent.querySelector(".scomponent-options."+pTo).lastChild);
             pItem.classList.remove("selected");
         });
         this.parent.querySelectorAll(".scomponent-options.selected .option").forEach(function(pItem){
@@ -50,6 +54,7 @@ Class.define(SelectComponent, [EventDispatcher], {
         this.parent.querySelectorAll(".scomponent-options.available .option").forEach(function(pItem){
             document.querySelector('#'+pItem.dataset.id).selected = false;
         });
+        this._reorder(e, 0);
     },
     select:function(e){
         this._switch(e, "available", "selected");
@@ -57,7 +62,45 @@ Class.define(SelectComponent, [EventDispatcher], {
     unselect:function(e){
         this._switch(e, "selected", "available");
     },
+    _reorder:function(e, pDirection){
+        e.preventDefault();
+        var options = this.parent.querySelectorAll(".scomponent-options.selected .option");
+        var option, ref_option, parent,i ;
+        if(pDirection !== 0){
+            i = pDirection === -1 ? 1 : options.length-2;
+            var inc = -pDirection;
+            for(;pDirection === -1 ? i<options.length : i>= 0; i += inc){
+                options = this.parent.querySelectorAll(".scomponent-options.selected .option");
+                option = options.item(i);
+                ref_option = options.item(i + pDirection);
+                if(option.classList.contains("selected")&&!ref_option.classList.contains("selected")){
+                    parent = option.parentNode;
+                    parent.removeChild(option);
+                    parent.insertBefore(option, pDirection===-1?ref_option:ref_option.nextSibling);
+                }
+            }
+            options = this.parent.querySelectorAll(".scomponent-options.selected .option");
+        }
+        var id, opt;
+        for(i = options.length-1; i>=0; i--){
+            option = options.item(i);
+            id = option.dataset.id;
+            opt = document.getElementById(id);
+            parent = opt.parentNode;
+            parent.removeChild(opt);
+            parent.insertBefore(opt, parent.firstChild);
+        }
+    },
+    up:function(e){
+        this._reorder(e, -1);
+    },
+    down:function(e){
+        this._reorder(e, 1);
+    },
     toggleOptionSelectionHandler:function(e){
+        if(!e.ctrlKey){
+            Array.from(e.target.parentNode.querySelectorAll(".option")).forEach(function(pElement){pElement.classList.remove("selected");});
+        }
         e.target.classList.toggle("selected");
     }
 });
